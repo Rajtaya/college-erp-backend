c6680cff7d942e43e76724814bfd6c9d56be9d2c
Rajtaya <Rajtaya@kuk.ac.in>
2026-03-21 13:24:52 +0530

    Add DB connection logging

--- db.js
+++ db.js
@@ -2,22 +2,23 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 let pool;
-
 const dbUrl = process.env.DATABASE_URL || process.env.MYSQL_URL;
 
 if (dbUrl) {
+  console.log('Using DATABASE_URL connection');
   const url = new URL(dbUrl);
   pool = mysql.createPool({
     host: url.hostname,
-    port: url.port || 3306,
-    user: url.username,
-    password: url.password,
+    port: parseInt(url.port) || 3306,
+    user: decodeURIComponent(url.username),
+    password: decodeURIComponent(url.password),
     database: url.pathname.replace('/', ''),
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
   });
 } else {
+  console.log('Using individual DB env vars, host:', process.env.DB_HOST);
   pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
